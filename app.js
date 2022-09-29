a94bb132443fea51c26f4efc15b43098a01594dd
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-09-29 23:50:41 +0600

    source code added

---
+++ app.js
@@ -0,0 +1,64 @@
+// mobile nav section 
+         const menuBtn = document.querySelector(".menu-icon span");
+         const searchBtn = document.querySelector(".search-icon");
+         const cancelBtn = document.querySelector(".cancel-icon");
+         const items = document.querySelector(".nav-items");
+         const form = document.querySelector("form");
+         menuBtn.onclick = ()=>{
+           items.classList.add("active");
+           menuBtn.classList.add("hide");
+           searchBtn.classList.add("hide");
+           cancelBtn.classList.add("show");
+         }
+         cancelBtn.onclick = ()=>{
+           items.classList.remove("active");
+           menuBtn.classList.remove("hide");
+           searchBtn.classList.remove("hide");
+           cancelBtn.classList.remove("show");
+           form.classList.remove("active");
+           cancelBtn.style.color = "#002482";
+         }
+
+// slide show
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("slide_dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+// slide show end
+
+// reviews 
+
+const review=document.querySelectorAll('.review_container');
+let i=0;
+let next=()=>{
+  review[i].classList.remove("review_active")
+  i=(i+1)%review.length;
+  review[i].classList.add("review_active")
+}
+let prev=()=>{
+  review[i].classList.remove("review_active")
+  i=(i-1+review.length)%review.length;
+  review[i].classList.add("review_active")
+}
